75351010265893fefe9d6821eea2c691c03ed77d
Pierre-Luc Doyon <p-l.doyon@hotmail.com>
2021-10-31 13:05:31 -0400

    version2

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+project_version=0.0.1
 echo "running login"
 docker login -u "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 echo "running push"
+docker tag log680-lab2 "$DOCKER_USERNAME"/log680-lab2:$project_version
+docker tag log680-lab2 "$DOCKER_USERNAME"/log680-lab2:latest
 docker push --all-tags "$DOCKER_USERNAME"/log680-lab2
 echo "good"
