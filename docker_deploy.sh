386af1a3e2db68e3c327c2ccb6a96de5287cf616
Pierre-Luc Doyon <p-l.doyon@hotmail.com>
2021-10-31 12:58:14 -0400

    all-tags

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,5 +2,5 @@
 echo "running login"
 docker login -u "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 echo "running push"
-docker push "$DOCKER_USERNAME"/log680-lab2:latest
+docker push --all-tags "$DOCKER_USERNAME"/log680-lab2
 echo "good"
