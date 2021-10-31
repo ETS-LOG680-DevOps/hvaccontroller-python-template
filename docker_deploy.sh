bb713501e2544b39d6b9e0c25fe416a05760eebb
Pierre-Luc Doyon <p-l.doyon@hotmail.com>
2021-10-31 13:24:26 -0400

    tag4

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -5,5 +5,6 @@ docker login -u "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 echo "running push"
 docker image tag projetsplets1/log680-lab2 "$DOCKER_USERNAME"/log680-lab2:$project_version
 docker image tag projetsplets1/log680-lab2 "$DOCKER_USERNAME"/log680-lab2:latest
-docker image push -a "$DOCKER_USERNAME"/log680-lab2
+docker image push "$DOCKER_USERNAME"/log680-lab2:$project_version
+docker image push "$DOCKER_USERNAME"/log680-lab2:latest
 echo "good"
