1af87edfda5f866156001c27d3caf4d9229c928c
Pierre-Luc Doyon <p-l.doyon@hotmail.com>
2021-10-31 15:20:26 -0400

    liniting src et test

--- src/main.py
+++ src/main.py
@@ -1,52 +1,57 @@
-from signalrcore.hub_connection_builder import HubConnectionBuilder
+"""This sript is the main program for Oxygène HVAC"""
+
 import logging
 import sys
-import requests
 import json
 import time
 import os
+import requests
+from signalrcore.hub_connection_builder import HubConnectionBuilder
 # import mysql.connector as mysql
 
 class Main:
+    """Main Class"""
     def __init__(self):
         self._hub_connection = None
-        self.HOST = os.environ["HVAC_HOST"]
+        self.host = os.environ["HVAC_HOST"]
 
-        if os.getenv("HVAC_TOKEN") == None:
-            self.TOKEN = "NOT_FOUND"
+        if os.getenv("HVAC_TOKEN") is None:
+            self.token = "NOT_FOUND"
         else:
-            self.TOKEN = os.environ["HVAC_TOKEN"]
+            self.token = os.environ["HVAC_TOKEN"]
 
-        if os.getenv("HVAC_COLD_LIMIT") == None:
-            self.COLD_LIMIT = 20
+        if os.getenv("HVAC_COLD_LIMIT") is None:
+            self.cold_limit = 20
         else :
-            self.COLD_LIMIT = os.environ["HVAC_COLD_LIMIT"]
+            self.cold_limit = os.environ["HVAC_COLD_LIMIT"]
 
-        if os.getenv("HVAC_HOT_LIMIT") == None:
-            self.HOT_LIMIT = 80
+        if os.getenv("HVAC_HOT_LIMIT") is None:
+            self.hot_limit = 80
         else :
-            self.HOT_LIMIT = os.environ["HVAC_HOT_LIMIT"]
+            self.hot_limit = os.environ["HVAC_HOT_LIMIT"]
 
-        if os.getenv("HVAC_TICKS") == None:
-            self.TICKS = 6
+        if os.getenv("HVAC_TICKS") is None:
+            self.ticks = 6
         else :
-            self.TICKS = os.environ["HVAC_TICKS"]
+            self.ticks = os.environ["HVAC_TICKS"]
 
     def __del__(self):
-        if self._hub_connection != None:
+        if self._hub_connection is not None:
             self._hub_connection.stop()
 
     def setup(self):
-        self.setSensorHub()
+        """Setup function"""
+        self.set_sensor_hub()
 
     def start(self):
-        if self.TOKEN == "NOT_FOUND":
+        """Start function"""
+        if self.token == "NOT_FOUND":
             print("No token found ! The program cannot be executed !")
             sys.exit(0)
 
         self.setup()
         self._hub_connection.start()
-        
+
         print("Press CTRL+C to exit.")
         while True:
             time.sleep(2)
@@ -54,9 +59,10 @@ class Main:
         self._hub_connection.stop()
         sys.exit(0)
 
-    def setSensorHub(self):
+    def set_sensor_hub(self):
+        """Function to connect to the server."""
         self._hub_connection = HubConnectionBuilder()\
-        .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")\
+        .with_url(f"{self.host}/SensorHub?token={self.token}")\
         .configure_logging(logging.INFO)\
         .with_automatic_reconnect({
             "type": "raw",
@@ -65,30 +71,34 @@ class Main:
             "max_attempts": 999
         }).build()
 
-        self._hub_connection.on("ReceiveSensorData", self.onSensorDataReceived)
+        self._hub_connection.on("ReceiveSensorData", self.on_sensor_data_received)
         self._hub_connection.on_open(lambda: print("||| Connection opened."))
         self._hub_connection.on_close(lambda: print("||| Connection closed."))
-        self._hub_connection.on_error(lambda data: print(f"||| An exception was thrown closed: {data.error}"))
+        self._hub_connection.on_error(lambda data: print(
+            f"||| An exception was thrown closed: {data.error}"))
 
-    def onSensorDataReceived(self, data):
-        try:        
+    def on_sensor_data_received(self, data):
+        """Function to extract the date and the temperature when we reveive data"""
+        try:
             print(data[0]["date"]  + " --> " + data[0]["data"])
-            date = data[0]["date"]
-            dp = float(data[0]["data"])
+            temperature = float(data[0]["data"])
 
-            self.analyzeDatapoint(date, dp)
+            self.analyze_datapoint(temperature)
+        # pylint: disable=broad-except
         except Exception as err:
             print(err)
-    
-    def analyzeDatapoint(self, date, data):
-        if data >= float(self.HOT_LIMIT):                
-            self.sendActionToHvac(date, "TurnOnAc", self.TICKS)
-        elif (data <= float(self.COLD_LIMIT)):                
-            self.sendActionToHvac(date, "TurnOnHeater", self.TICKS)
 
-    def sendActionToHvac(self, date, action):
-        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{self.TICKS}") 
-        details = json.loads(r.text)
+    def analyze_datapoint(self, data):
+        """Function to activate the AC or heater if the temperature is too high or too low"""
+        if data >= float(self.hot_limit):
+            self.send_action_to_hvac("TurnOnAc", self.ticks)
+        elif data <= float(self.cold_limit):
+            self.send_action_to_hvac("TurnOnHeater", self.ticks)
+
+    def send_action_to_hvac(self, action, ticks):
+        """Function to send order to the hvac"""
+        request = requests.get(f"{self.host}/api/hvac/{self.token}/{action}/{ticks}")
+        details = json.loads(request.text)
         print(details)
 
 if __name__ == '__main__':
--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,8 @@
+"""This script is used to test the function in main.py"""
+
 import unittest
-import sys
-import requests
-import json
 import os
+import requests
 from src.main import Main
 # import mysql.connector as mysql
 
@@ -13,27 +13,32 @@ HOT_LIMIT = "80"
 COLD_LIMIT = "20"
 
 class TestStringMethods(unittest.TestCase):
-
+    """Test class for the Main class"""
     def test_simulator_up(self):
-        r = requests.get(f"{HOST}/api/health") 
-        self.assertEqual("All system operational Commander !", r.text)
-    
+        """Function to test if we receive an information from the server"""
+        request = requests.get(f"{HOST}/api/health")
+        self.assertEqual("All system operational Commander !", request.text)
+
     def test_token(self):
+        """Test if the token in the environment variable is good"""
         hvac = Main()
-        self.assertEqual(TOKEN, hvac.TOKEN)
-    
+        self.assertEqual(TOKEN, hvac.token)
+
     def test_hot_limit(self):
+        """Test if the hot limit is different from the default value"""
         hvac = Main()
-        self.assertNotEqual(HOT_LIMIT, hvac.HOT_LIMIT)
+        self.assertNotEqual(HOT_LIMIT, hvac.hot_limit)
 
 
     def test_cold_limit(self):
+        """Test if the cold limit is different from the default value"""
         hvac = Main()
-        self.assertNotEqual(COLD_LIMIT, hvac.COLD_LIMIT)
+        self.assertNotEqual(COLD_LIMIT, hvac.cold_limit)
 
     def test_ticks(self):
+        """Test if the number of ticks is different from the default value"""
         hvac = Main()
-        self.assertNotEqual(TICKS, hvac.TICKS)
-    
+        self.assertNotEqual(TICKS, hvac.ticks)
+
 if __name__ == '__main__':
     unittest.main()
