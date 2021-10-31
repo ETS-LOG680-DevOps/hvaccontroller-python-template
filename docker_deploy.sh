616e34d71fb7e3b3027fbe902d83d4c1ed695f82
Pierre-Luc Doyon <p-l.doyon@hotmail.com>
2021-10-31 12:17:57 -0400

    modif docker_deploy2

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker login -u "$DOCKER_USERNAME" --password  "$DOCKER_PASSWORD"
+echo "running login"
+docker login -u "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+echo "running push"
 docker push "$DOCKER_USERNAME"/log680-lab2:latest
+echo "good"
