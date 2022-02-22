a1b0d9c7d1db81a30dac829197b0bc8bbbdc8bdb
David <davidchi01@protonmail.com>
2022-02-22 14:57:49 -0500

    added Dockerfile

--- test/test_main.py
+++ test/test_main.py
@@ -6,13 +6,13 @@ import os
 from src.main import Main
 import mysql.connector as mysql
 
-HOST = os.environ["HVAC_HOST"]
+HOST = os.getenv("HVAC_HOST",'http://178.128.234.252:32775/')
 
 class TestStringMethods(unittest.TestCase):
 
     def test_simulator_up(self):
         r = requests.get(f"{HOST}/api/health") 
-        self.assertEqual("All system operational Commander !", r.text)
+        self.assertEqual("All system operational Commander !!", r.text)
     
 if __name__ == '__main__':
     unittest.main()
